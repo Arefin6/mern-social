8f138f3ccc234197b011b2a0881929fffdb28677
Arefi6 <arefinhossain3@gmail.com>
2021-05-28 20:34:21 +0600

    all Pages Degine done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Home from "./pages/home/Home";
+import Profile from './pages/profile/Profile';
+import Login from './pages/login/Login';
 
 
 function App() {
   return (
     <div>
-      <Home></Home>
-      
+       <Home></Home>
+     
     </div>
   );
 }
